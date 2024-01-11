45339ba1c56a86308b984de590c3b8075595ad59
Hideto Ozeki <Ozeki-20.0104@outlook.jp>
2024-01-11 12:08:20 +0900

    test sql update

--- src/SQLs/CREATE.sql
+++ src/SQLs/CREATE.sql
@@ -23,18 +23,20 @@ CREATE TABLE comment(
     comment_text VARCHAR(240) NOT NULL,
     FOREIGN KEY(post_id) REFERENCES post(post_id),
     FOREIGN KEY(user_id) REFERENCES user(user_id),
-    PRIMARY KEY(comment_id);
+    PRIMARY KEY(comment_id)
 );
 
 CREATE TABLE vote_option(
     post_id INTEGER NOT NULL,
-    vote_option_id INTEGER AUTO_INCREMENT,
+    vote_option_id INTEGER NOT NULL,
     option_name VARCHAR(100) NOT NULL,
-    order INTEGER NOT NULL,
+    option_order INTEGER NOT NULL,
     image_path VARCHAR(100),
     FOREIGN KEY (post_id) REFERENCES post(post_id),
-    PRIMARY KEY (post_id,vote_option_id);
+    PRIMARY KEY (post_id,vote_option_id)
 );
+
+INSERT INTO vote_option VALUES(投稿ID,COALESCE((select MAX(vote_option_id)+1 from vote_option where post_id = 投稿ID),1),選択名,選択肢番号,画像パス);
 
 CREATE TABLE vote (
     vote_id INTEGER AUTO_INCREMENT,
@@ -43,5 +45,5 @@ CREATE TABLE vote (
     vote_number INTEGER NOT NULL,
     PRIMARY KEY(vote_id),
     FOREIGN KEY(post_id) REFERENCES post(post_id),
-    FOREIGN KEY(user_id) REFERENCES post(user_id);
+    FOREIGN KEY(user_id) REFERENCES post(user_id)
 );
--- src/SQLs/testdata.sql
+++ src/SQLs/testdata.sql
@@ -1,6 +1,11 @@
-INSERT INTO user VALUES("none_the_wiser","poster1@outlook.com","12345");
+INSERT INTO user(user_alias,mail_address,user_password) VALUES("none_the_wiser","poster1@outlook.com","12345");
 
-INSERT INTO post VALUES(1,1,"アフリカ","アフリカは国それとも大陸？","アフリカは大陸です");
+INSERT INTO post(user_id,post_type,post_title,post_description,post_explanation) VALUES(1,1,"アフリカ","アフリカは国それとも大陸？","アフリカは大陸です");
 
-INSERT INTO vote_option VALUES(1,"国",1,"test1.png");
-INSERT INTO vote_option VALUES(1,"大陸",2,"test2.png");
+INSERT INTO vote_option VALUES(1,1,"国",1,"test1.png");
+INSERT INTO vote_option VALUES(1,(select COUNT(*)+1 from vote_option where post_id = 1),"国",1,"test1.png");
+INSERT INTO vote_option VALUES(1,COALESCE((select MAX(vote_option_id)+1 from vote_option where post_id = 1) as temp1,1),"大陸",2,"test2.png");
+
+INSERT INTO vote_option VALUES(2,1,"国",1,"test1.png");
+INSERT INTO vote_option VALUES(2,2,"国",1,"test1.png");
+INSERT INTO vote_option VALUES(2,3,"国",1,"test1.png");
